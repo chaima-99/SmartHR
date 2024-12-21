74d79340b64a450dd3413017cac2a8b8f712ce7b
NorIslam04 <islamaoudi18@gmail.com>
2024-12-21 10:52:14 +0100

    add haching function for password

--- app/crud.py
+++ app/crud.py
@@ -1,15 +1,19 @@
 from fastapi import HTTPException
 from sqlalchemy.orm import Session
 from app import models, schemas
+from passlib.context import CryptContext
 
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
 
 def create_admin(db: Session, admin: schemas.Admin):
     existing_admin = db.query(models.Admin).filter(models.Admin.username == admin.username).first()
     if existing_admin:
         raise HTTPException(status_code=400, detail="Email already registered")
     
-    admin = models.Admin(username=admin.username, password=admin.password)
+    admin = models.Admin(username=admin.username, password=hash_password(admin.password))
     db.add(admin)
     db.commit()
     db.refresh(admin)
